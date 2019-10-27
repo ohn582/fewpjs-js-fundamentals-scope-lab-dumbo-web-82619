c5ef04ff2ba93861c9e54b8b740869629727fc8e
Neal JIn Oh <no4259@gmail.com>
2019-10-27 23:19:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-animal = ""
+animal = "cat"
 
 function myAnimal() {
   return animal
@@ -12,8 +12,6 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  let two = 2
   return n + two
-
-  // Feel free to move things around!
-  two = 2
 }
